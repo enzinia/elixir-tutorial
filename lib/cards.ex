3c82e644d3273c76095ba44077910e6b02ef9b5d
enzinia <vkukovskij@gmail.com>
2020-02-19 20:54:27 +0900

    Add suit to cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,18 @@
 defmodule Cards do
   def create_deck do
-    ["1", "2", "3"]
+    values = ["A", "2", "3", "4", "5"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle deck
+  end
+
+  def contains?(deck, card) do
+    Enum.member? deck, card
   end
 end
